2906c5863faacb5301bcc32f65e38555df88e4bb
Allen Thomas <allenthom05@gmail.com>
2024-04-12 14:33:06 -0400

    Changes to TagCloud

---
+++ test/ProgramTest.java
@@ -0,0 +1,92 @@
+
+
+/**
+ * JUnit test fixture for 
+ *
+ * @author 
+ * @author 
+ *
+ */
+public abstract class ProgramTest {
+
+    /**
+     * The name of a file containing a BL program.
+     */
+    private static final String FILE_NAME_1 = "projects/07-program-and-statement/data/program-sample.bl";
+
+    // TODO - define file names for additional test inputs
+
+    /**
+     * Invokes the {@code Program} constructor for the implementation under test
+     * and returns the result.
+     *
+     * @return the new program
+     * @ensures constructor = ("Unnamed", {}, compose((BLOCK, ?, ?), <>))
+     */
+    protected abstract Program constructorTest();
+
+    /**
+     * Invokes the {@code Program} constructor for the reference implementation
+     * and returns the result.
+     *
+     * @return the new program
+     * @ensures constructor = ("Unnamed", {}, compose((BLOCK, ?, ?), <>))
+     */
+    protected abstract Program constructorRef();
+
+    /**
+     *
+     * Creates and returns a {@code Program}, of the type of the implementation
+     * under test, from the file with the given name.
+     *
+     * @param filename
+     *            the name of the file to be parsed to create the program
+     * @return the constructed program
+     * @ensures createFromFile = [the program as parsed from the file]
+     */
+    private Program createFromFileTest(String filename) {
+        Program p = this.constructorTest();
+        SimpleReader file = new SimpleReader1L(filename);
+        p.parse(file);
+        file.close();
+        return p;
+    }
+
+    /**
+     *
+     * Creates and returns a {@code Program}, of the reference implementation
+     * type, from the file with the given name.
+     *
+     * @param filename
+     *            the name of the file to be parsed to create the program
+     * @return the constructed program
+     * @ensures createFromFile = [the program as parsed from the file]
+     */
+    private Program createFromFileRef(String filename) {
+        Program p = this.constructorRef();
+        SimpleReader file = new SimpleReader1L(filename);
+        p.parse(file);
+        file.close();
+        return p;
+    }
+
+    /**
+     * Test constructor.
+     */
+    @Test
+    public final void testConstructor() {
+        /*
+         * Setup
+         */
+        Program pRef = this.constructorRef();
+
+        /*
+         * The call
+         */
+        Program pTest = this.constructorTest();
+
+        /*
+         * Evaluation
+         */
+        assertEquals(pRef, pTest);
+    }
